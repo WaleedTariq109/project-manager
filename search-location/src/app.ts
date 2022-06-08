7eda2df579f30d4874bbcc995e09bfbf3d2d91f2
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-06-09 03:32:00 +0500

    Get entered address

--- search-location/src/app.ts
+++ search-location/src/app.ts
@@ -1,1 +1,10 @@
-// Code goes here!
+const form = <HTMLFormElement>document.querySelector('form');
+const addressInput = <HTMLInputElement>document.querySelector('#address');
+
+function searchHandler(e: Event) {
+  e.preventDefault();
+  const address = addressInput.value;
+  console.log(address);
+}
+
+form.addEventListener('submit', searchHandler);
