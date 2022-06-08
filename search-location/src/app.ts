e266ed65b17fbdb57d376ceb8bea36f97848ece8
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-06-09 04:13:46 +0500

    Add types for google map api

--- search-location/src/app.ts
+++ search-location/src/app.ts
@@ -5,26 +5,32 @@ const addressInput = <HTMLInputElement>document.querySelector('#address');
 
 const GOOGLE_API_KEY = 'AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU';
 
-type GoogleResponse = {
-  results: { geometry: { location: { lat: number; lng: number } } }[];
-  status: 'OK' | 'ZERO_RESULTS';
-};
+// type GoogleResponse = {
+//   results: { geometry: { location: { lat: number; lng: number } } }[];
+//   status: 'OK' | 'ZERO_RESULTS';
+// };
 
 function searchHandler(e: Event) {
   e.preventDefault();
   const address = addressInput.value;
 
   axios
-    .get<GoogleResponse>(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)},+CA&key=${GOOGLE_API_KEY}`
-    )
+    .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)},+CA&key=${GOOGLE_API_KEY}`)
     .then((res) => {
+      console.log(res);
       if (res.data.status !== 'OK') {
-        throw new Error('Could not fetch location!');
+        throw new Error(res.data.error_message);
       }
-
       const coords = res.data.results[0].geometry.location;
-      console.log(coords);
+
+      const map = new google.maps.Map(<HTMLDivElement>document.getElementById('map'), {
+        center: coords,
+        zoom: 8,
+      });
+      new google.maps.Marker({
+        position: coords,
+        map: map,
+      });
     })
     .catch((err) => {
       alert(err.message);
